2b1c4fddb0561ae8e4f2eddd178cd341661cbcbf
Ubaldo Cotta <ucotta@me.com>
2016-10-26 00:19:33 +0300

    edependencies

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0)
-		.Package(url: "https://github.com/ucotta/Session-MySQL.git", majorVersion: 0)
+		.Package(url: "git@github.com:ucotta/MySQL-ConnectionPool.git", majorVersion: 0, minor: 20),
+		.Package(url: "git@github.com:ucotta/Session-MySQL.git",  majorVersion: 0)
     ]
 )
 
