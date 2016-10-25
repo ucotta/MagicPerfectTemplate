99e7d80a0e7870c622c26922987fc969f323a61f
Ubaldo Cotta <ucotta@me.com>
2016-10-26 00:18:02 +0300

    edependencies

--- Package.swift
+++ Package.swift
@@ -2,11 +2,13 @@
 import PackageDescription
 
 let package = Package(
-	name: "TestingPerfectTemplate",
+	name: "PaellaPerfectTemplate",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-		.Package(url: "git@github.com:ucotta/Session-MySQL.git",  majorVersion: 0)
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", majorVersion: 2, minor: 0)
+		.Package(url: "https://github.com/ucotta/Session-MySQL.git", majorVersion: 0)
     ]
 )
 
