bf398dbd63f37a3b404287275c26edec7ff8e3f7
Ubaldo Cotta <ucotta@me.com>
2016-11-03 03:45:50 +0200

    Change name project
    Yes, paella was not the very best in the world... :D

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "PaellaPerfectTemplate",
+	name: "MagicPerfectTemplate",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
